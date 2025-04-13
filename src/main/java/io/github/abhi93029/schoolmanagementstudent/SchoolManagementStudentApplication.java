28e2efa33d05bae212dfc7083f6afacdfc9ee6e3
Abhi93029 <rajathakuryt@gmail.com>
2025-04-13 17:05:14 +0530

    Initial commit

---
+++ src/main/java/io/github/abhi93029/schoolmanagementstudent/SchoolManagementStudentApplication.java
@@ -0,0 +1,13 @@
+package io.github.abhi93029.schoolmanagementstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementStudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/abhi93029/schoolmanagementstudent/SchoolManagementStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.abhi93029.schoolmanagementstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
