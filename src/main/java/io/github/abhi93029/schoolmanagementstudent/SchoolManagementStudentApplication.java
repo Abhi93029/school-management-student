1b68b0025e82e03a11884d75ec7a75871f9dc8d4
Abhi93029 <rajathakuryt@gmail.com>
2025-04-13 22:28:16 +0530

    Initial commit

--- src/main/java/io/github/abhi93029/schoolmanagementstudent/SchoolManagementStudentApplication.java
+++ src/main/java/io/github/abhi93029/schoolmanagementstudent/SchoolManagementStudentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SchoolManagementStudentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SchoolManagementStudentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementStudentApplication.class, args);
+    }
 
 }
